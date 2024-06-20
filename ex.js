e838545059831dabab0bdc3bc363c3e0f952df2b
MTE <mateus_espindola1@estudante.sesisenai.org.br>
2024-06-19 21:31:20 -0300

    primeiros exercicios java script

---
+++ ex.js
@@ -0,0 +1,49 @@
+//n1
+let nome = 'jão'
+console.log(nome)
+
+//n2
+let a = 5 ; let b = 10; let resultado = a + b
+console.log (resultado)
+
+//n3
+let primeironome = 'josefa' ;
+let ultimonome = ' silva';
+let nomeintero = primeironome + ultimonome
+console.log(nomeintero)
+
+//n4
+let pessoa = {
+    nome : 'mateus',
+     idade : 16,
+     endereço : 'rua pedrosa',
+telefone : 123456765
+}
+console.log (pessoa)
+
+//n5
+let fruta = ['maçã', 'banana', 'laranja']
+fruta.push('uva')
+console.log (fruta)
+
+//n6
+let numeros = [1 , 2 , 3 , 4 , 5];
+numeros.push(5);
+numeros.push(1);
+console.log (numeros)
+
+//n6
+let cores = ['vermelho', 'verde', 'azul']
+ console.log (cores)
+
+ for (i=0; i<cores.length; i++){
+    console.log(cores[i])
+ }
+
+ //n7
+ let  idade = 16
+ if (idade >= 18) { 
+    console.log ('você é maior de 18 anos')
+ } else {
+    console. log ('você tem menos de 18 anos')
+}
