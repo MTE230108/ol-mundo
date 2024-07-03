655cf1757ad40c0b879c36961aff7907ff24df9e
MTE <mateus_espindola1@estudante.sesisenai.org.br>
2024-07-03 19:38:44 -0300

    funcao

---
+++ funcoes.js
@@ -0,0 +1,30 @@
+function soma (n1 , n2){
+    let soma = n1 + n2
+    return soma
+}
+console.log (soma (2,3))
+
+
+{
+
+let senhacorreta = "Mte"}
+
+function comparasenha (senhacorreta, senhaesctrita){
+if(senhaesctrita == senhacorreta){
+console.log("conectado")
+} else {
+    console.log("senha incorreta")
+}
+    }
+    comparasenha(senhacorreta, 'Hue')
+    comparasenha(senhacorreta, 'Mte')
+
+
+    //exercicio 1
+    function fatorialc (){
+            let resultado =1
+        for (let i= 1 ; i < fatorial ; i ++){
+            resultado = fatorial * i
+        }   return resultado
+    }
+    console.log (resultado)
